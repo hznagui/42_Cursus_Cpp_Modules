09d55a1c354672fa2791f5dfe9a1eb824a69d0b7
Houdaifa Znagui <hznagui@e2r9p5.1337.ma>
2023-10-15 17:06:07 +0100

    testing

--- cpp00/ex01/test.cpp
+++ cpp00/ex01/test.cpp
@@ -6,7 +6,7 @@
 /*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 11:00:32 by hznagui           #+#    #+#             */
-/*   Updated: 2023/10/15 16:49:11 by hznagui          ###   ########.fr       */
+/*   Updated: 2023/10/15 17:05:29 by hznagui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 #include <stdio.h>
 
 
-class PhoneBook
-{
+// class PhoneBook
+// {
     
-}
+// }
 
 class Contact
 {
@@ -27,23 +27,37 @@ class Contact
     std::string phone_number;
     std::string darkest_secret;
     public:
+    void set_first_name(std::string a){first_name=a;}
+    std::string get_first_name(void){return first_name;}
     Contact();
     ~Contact();
 };
 Contact::Contact()
     {
-        std::cout << "first_name : ";
-        std::getline(std::cin, first_name);
-        if (! first_name[0])
-            std::cout << "kteb xi haja al 9lawi!"<< std::endl;
+        
     } 
 Contact::~Contact()
     {
         std::cout << "sf salat" << std::endl;
-    }     
+    }
+    
+
+int add(Contact *a)
+{
+    std::string str;
+            std::cout << "first_name : ";
+        std::getline(std::cin, str);
+        if (! str[0])
+            return ( std::cout << "kteb xi haja al 9lawi!"<< std::endl,1);
+        else
+            a->set_first_name(str);
+        std::cout<< a->get_first_name() <<std::endl;
+    return 0; 
+}  
 int main()
 {
     Contact a1;
+    add(&a1);
     // std::cout <<"a1 = "<< a1.a << " a2 = "<< a2.a << std::endl;
     // a1.a++;
     // std::cout <<"a1 = "<< a1.a << " a2 = "<< a2.a << std::endl;
