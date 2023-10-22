17215dd58f5e16089ff570add4e6207037a1c465
hznagui <houdayfahouayfa7@gmail.com>
2023-10-22 10:54:50 +0100

    done deal

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 17:12:59 by hznagui           #+#    #+#             */
-/*   Updated: 2023/10/21 14:06:12 by hznagui          ###   ########.fr       */
+/*   Updated: 2023/10/22 10:54:33 by hznagui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void randomChump( std::string name )
     Zombie *ret = newZombie(name);
     if (!ret)
     {
-        std::cout << "problem in allocation"<< std::endl;
+        std::cout <<name<< " problem in allocation"<< std::endl;
         return;
     }
     ret->announce();
@@ -35,6 +35,8 @@ void randomChump( std::string name )
 int main ()
 {
     Zombie *p1=newZombie("tom"),*p2=newZombie("jerry");
+    if (!p1 || !p2)
+        return 1;
     p1->announce();
     p2->announce();
     randomChump("sa3id");
