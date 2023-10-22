e6d97578c254b7ea45892c7d3efcf02bdda7945d
hznagui <houdayfahouayfa7@gmail.com>
2023-10-22 16:00:39 +0100

    done deal

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 17:12:59 by hznagui           #+#    #+#             */
-/*   Updated: 2023/10/22 12:07:34 by hznagui          ###   ########.fr       */
+/*   Updated: 2023/10/22 16:00:25 by hznagui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,13 @@
 int main ()
 {
 std::string string ="HI THIS IS BRAIN";
+std::string stringPTR=string;
+std::string &stringREF=string;
+std::cout << "string adress: "<<&string << std::endl ;
+std::cout << "string pointer adress: "<<&stringPTR << std::endl ;
+std::cout << "string reference adress: "<<&stringREF << std::endl ;
+
+std::cout << "The value of the string variable: "<<string << std::endl ;
+std::cout << "The value pointed to by stringPTR: "<<stringPTR << std::endl ;
+std::cout << "The value pointed to by stringREF: "<<stringREF << std::endl ;
 }
