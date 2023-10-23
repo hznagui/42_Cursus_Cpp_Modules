7b6339df415b4abdcc1fdfacc386cbfa3e108723
hznagui <houdayfahouayfa7@gmail.com>
2023-10-23 12:56:51 +0100

    done deal

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,9 +6,17 @@
 /*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 11:41:26 by hznagui           #+#    #+#             */
-/*   Updated: 2023/10/23 11:41:59 by hznagui          ###   ########.fr       */
+/*   Updated: 2023/10/23 11:49:53 by hznagui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
+int main(int argc, char **argv)
+{
+    if (argc != 4)
+    {
+        
+    }
+    std::cout << "we need 3 argement please !"<<std::endl;
+}
