152db0041d6692eb781dbf294b4140703a72ef0e
hznagui <houdayfahouayfa7@gmail.com>
2023-11-01 11:12:05 +0100

    done deal

---
+++ cpp02/ex00/Fixed.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/30 16:13:55 by hznagui           #+#    #+#             */
+/*   Updated: 2023/11/01 11:10:00 by hznagui          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+class Fixed
+{
+    int number_value;
+    static const int fractional_bits;
+    public:
+    Fixed();
+    Fixed(Fixed &obj);
+    Fixed &operator = ( Fixed &obj);
+    ~Fixed();
+};
+
+#endif
