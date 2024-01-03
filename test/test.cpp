de15a9ece21bc56b28f525b78b9e14eff868dde4
hznagui <houdayfahouayfa7@gmail.com>
2024-01-03 22:22:09 +0100

    done deal

--- test/test.cpp
+++ test/test.cpp
@@ -1,27 +1,13 @@
-// C++ program to demonstrate the
-// use of a stringstream to
-// convert int to string
 #include <iostream>
-#include <sstream>
-using namespace std;
+#include <cstdlib>
 
-// static_cast_Operator.cpp
-// compile with: /LD
-class B {virtual void d(){}};
+int main() {
+    std::string stringValue = "3.14ff";
+    
+    // Use atof to convert string to float
+    float floatValue = static_cast<float>(atof(stringValue.c_str()));
 
-class D : public B {int y;};
+    std::cout << floatValue << std::endl;
 
-void f(B* pb, D* pd) {
-   D* pd2 = static_cast<D*>(pb);   // Not safe, D can have fields
-    if (!pd2)
-		cout << "makhedmxi\n";                               // and methods that are not in B.
-
-   B* pb2 = static_cast 	<B*>(pd);   // Safe conversion, D always
-                                   // contains all of B.
-}
-int main ()
-{
-	B a;
-	D c;
-	f(&a,&c);
+    return 0;
 }
