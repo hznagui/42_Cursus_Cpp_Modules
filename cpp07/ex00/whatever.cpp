d8630dd6525f97b51836f7be4ebe2fae867d19bf
hznagui <houdayfahouayfa7@gmail.com>
2024-01-08 17:43:40 +0100

    done deal

--- cpp07/ex00/whatever.cpp
+++ cpp07/ex00/whatever.cpp
@@ -6,8 +6,23 @@
 /*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 17:32:07 by hznagui           #+#    #+#             */
-/*   Updated: 2024/01/08 17:36:44 by hznagui          ###   ########.fr       */
+/*   Updated: 2024/01/08 17:42:09 by hznagui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
+
+template <class T> void  swap(T *b, T *a)
+{
+    T tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+template <class T> T  min(T b, T a)
+{
+    return (( a > b) ? b : a);
+}
+template <class T> T  max(T b, T a)
+{
+    return (( a < b) ? b : a);
+}
--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -6,14 +6,14 @@
 /*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 17:32:10 by hznagui           #+#    #+#             */
-/*   Updated: 2024/01/08 17:37:06 by hznagui          ###   ########.fr       */
+/*   Updated: 2024/01/08 17:38:08 by hznagui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
 #include <iostream>
-template <class T> swap(T *,T *);
+template <class T> void swap(T *,T *);
 
 template <class T> T min(T , T);
 
