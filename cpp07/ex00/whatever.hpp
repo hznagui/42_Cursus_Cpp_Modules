369d6d7ca752c844d50d7894d2a3bfeb2f23d9b2
hznagui <houdayfahouayfa7@gmail.com>
2024-01-08 17:37:32 +0100

    beging of ex00

---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/08 17:32:10 by hznagui           #+#    #+#             */
+/*   Updated: 2024/01/08 17:37:06 by hznagui          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+#include <iostream>
+template <class T> swap(T *,T *);
+
+template <class T> T min(T , T);
+
+template <class T> T max(T, T);
+#endif
