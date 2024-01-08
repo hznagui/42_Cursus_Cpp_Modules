1b7d6988b99c6c680c5a4b2f4bd068f119d27197
hznagui <houdayfahouayfa7@gmail.com>
2024-01-08 17:53:06 +0100

    done deal

--- cpp07/ex00/whatever.cpp
+++ cpp07/ex00/whatever.cpp
@@ -6,23 +6,9 @@
 /*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 17:32:07 by hznagui           #+#    #+#             */
-/*   Updated: 2024/01/08 17:42:09 by hznagui          ###   ########.fr       */
+/*   Updated: 2024/01/08 17:51:22 by hznagui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
 
-template <class T> void  swap(T *b, T *a)
-{
-    T tmp = *a;
-    *a = *b;
-    *b = tmp;
-}
-template <class T> T  min(T b, T a)
-{
-    return (( a > b) ? b : a);
-}
-template <class T> T  max(T b, T a)
-{
-    return (( a < b) ? b : a);
-}
--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -6,16 +6,28 @@
 /*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 17:32:10 by hznagui           #+#    #+#             */
-/*   Updated: 2024/01/08 17:38:08 by hznagui          ###   ########.fr       */
+/*   Updated: 2024/01/08 17:52:32 by hznagui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
+
 #include <iostream>
-template <class T> void swap(T *,T *);
 
-template <class T> T min(T , T);
+template <class T> void swap(T &b, T &a)
+{
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
+template <class T> T  min(T b, T a)
+{
+    return (( a > b) ? b : a);
+}
+template <class T> T  max(T b, T a)
+{
+    return (( a < b) ? b : a);
+}
 
-template <class T> T max(T, T);
 #endif
