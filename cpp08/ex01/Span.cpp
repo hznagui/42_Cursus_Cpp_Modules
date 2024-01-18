4ca3c35a6e8b218d2b5c4be6dced9d6d64cfeabb
hznagui <houdayfahouayfa7@gmail.com>
2024-01-18 11:52:27 +0100

    Makefile done

---
+++ cpp08/ex01/Span.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/18 11:39:39 by hznagui           #+#    #+#             */
+/*   Updated: 2024/01/18 11:51:21 by hznagui          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int main()
+{
+Span sp = Span(5);
+sp.addNumber(6);
+sp.addNumber(3);
+sp.addNumber(17);
+sp.addNumber(9);
+sp.addNumber(11);
+std::cout << sp.shortestSpan() << std::endl;
+std::cout << sp.longestSpan() << std::endl;
+return 0;
+}
