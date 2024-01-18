65c227b3e98981ae1a5bc582a8a56426c61de849
hznagui <houdayfahouayfa7@gmail.com>
2024-01-18 11:59:27 +0100

    working on header file

--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -6,10 +6,21 @@
 /*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 11:39:41 by hznagui           #+#    #+#             */
-/*   Updated: 2024/01/18 11:51:16 by hznagui          ###   ########.fr       */
+/*   Updated: 2024/01/18 11:55:16 by hznagui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
+class Span
+{
+    public:
+    Span();
+    Span(unsigned int);
+    Span(Span &);
+    Span &operator=(Span &);
+    void addNumber();
+    int  shortestSpan();
+    int  longestSpan();    
+};
