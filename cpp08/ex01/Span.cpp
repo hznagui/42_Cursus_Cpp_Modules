b60c351bd18fa8c452b0e3ce8b062973868a62a9
hznagui <houdayfahouayfa7@gmail.com>
2024-01-18 15:37:17 +0100

    before first tset

--- cpp08/ex01/Span.cpp
+++ cpp08/ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 11:39:39 by hznagui           #+#    #+#             */
-/*   Updated: 2024/01/18 13:43:10 by hznagui          ###   ########.fr       */
+/*   Updated: 2024/01/18 15:36:55 by hznagui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,45 @@ Span::Span(Span &obj)
 {
     size=obj.size;
 }
-Span &Span::Span(Span &obj)
+Span &Span::operator=(Span &obj)
 {
     size=obj.size;
+    return *this;
+}
+void Span::addNumber(int nbr)
+{
+    if (v.size() == size)
+        throw std::exception();
+    else
+        v.push_back(nbr);
+}
+void Span::assign(int nbr)
+{
+    v.assign(size,nbr);
+}
+int Span::shortestSpan()
+{
+    if (v.size()<=1)
+        throw std::exception();
+    std::vector<int> sorted = v;
+    std::sort(sorted.begin(),sorted.end());
+    int ret=0,tmp=0;
+    std::vector<int>::iterator begin=sorted.begin(), ending = begin+1;
+    while (begin != sorted.end())
+    {
+        tmp=*ending-*begin;
+        if (tmp < ret)
+            ret = tmp;
+        begin++;
+        ending++;
+    }
+    return ret;
+}
+int Span::longestSpan()
+{
+    if (v.size()<=1)
+        throw std::exception();
+    std::vector<int> sorted = v;
+    std::sort(sorted.begin(),sorted.end());
+    return sorted.back()-sorted.front();
 }
--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -6,7 +6,7 @@
 /*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 11:39:41 by hznagui           #+#    #+#             */
-/*   Updated: 2024/01/18 13:38:53 by hznagui          ###   ########.fr       */
+/*   Updated: 2024/01/18 15:00:48 by hznagui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ class Span
     std::vector<int> v;
     unsigned int size;
     public:
-    Span();
-    Span(unsigned int);
-    Span(Span &);
-    Span &operator=(Span &);
-    void addNumber();
-    void assign();
+    Span();//
+    Span(unsigned int);//
+    Span(Span &);//
+    Span &operator=(Span &);//
+    void addNumber(int);//
+    void assign(int);//
     int  shortestSpan();
     int  longestSpan();    
 };
