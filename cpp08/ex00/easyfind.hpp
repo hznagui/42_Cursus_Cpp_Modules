89b0b7ab2a30ee2dfb7ccbb2db520b4baa285e22
hznagui <houdayfahouayfa7@gmail.com>
2024-01-18 11:30:04 +0100

    done deal

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 10:34:17 by hznagui           #+#    #+#             */
-/*   Updated: 2024/01/18 11:20:25 by hznagui          ###   ########.fr       */
+/*   Updated: 2024/01/18 11:28:15 by hznagui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 #include <iostream>
 #include <algorithm>
+#include <vector>
 template <class T> std::string easyfind(T container,int find)
 {
-    (std::find(container.begin(),container.end(),find) != container.end()) ? return "is found" : return "it not found";
+    return (std::find(container.begin(),container.end(),find) != container.end()) ? ("is found") : ("it not found");
 }
