16a388664233961a41e079407720d8e8f1c2c8d1
hznagui <houdayfahouayfa7@gmail.com>
2024-01-19 11:23:47 +0100

    Makefile done

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 10:42:59 by hznagui           #+#    #+#             */
-/*   Updated: 2024/01/19 11:12:03 by hznagui          ###   ########.fr       */
+/*   Updated: 2024/01/19 11:20:35 by hznagui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,12 @@
 #include <list>
 #include <vector>
 
-template <class T> class MutantStack : public  std::stack< T, std::vector<T> >
+template <class T> class MutantStack : public  std::stack<T>
 {
     public:
     MutantStack(){}
-    MutantStack(const MutantStack &obj){(void)obj;}
-    ~MutantStack(){}
+    MutantStack(const MutantStack &obj)
+    {(void)obj;}
+    ~MutantStack() {}
     MutantStack &operator=(const MutantStack &obj){(void)obj;return *this;}
 };
