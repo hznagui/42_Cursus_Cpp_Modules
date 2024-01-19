e67150c35152ffa5d2419311814551599e9b8c3d
hznagui <houdayfahouayfa7@gmail.com>
2024-01-19 11:12:47 +0100

    one problem

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,11 +6,22 @@
 /*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 10:42:59 by hznagui           #+#    #+#             */
-/*   Updated: 2024/01/19 10:43:36 by hznagui          ###   ########.fr       */
+/*   Updated: 2024/01/19 11:12:03 by hznagui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
+#include <stack>
+#include <list>
+#include <vector>
 
+template <class T> class MutantStack : public  std::stack< T, std::vector<T> >
+{
+    public:
+    MutantStack(){}
+    MutantStack(const MutantStack &obj){(void)obj;}
+    ~MutantStack(){}
+    MutantStack &operator=(const MutantStack &obj){(void)obj;return *this;}
+};
