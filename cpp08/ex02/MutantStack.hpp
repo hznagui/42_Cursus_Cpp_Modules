cfb3715da665b328b094db6d64422542c2df7c6b
hznagui <houdayfahouayfa7@gmail.com>
2024-01-20 19:39:06 +0100

    done deal

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 10:42:59 by hznagui           #+#    #+#             */
-/*   Updated: 2024/01/20 16:56:26 by hznagui          ###   ########.fr       */
+/*   Updated: 2024/01/20 19:38:43 by hznagui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,21 @@
 #include <vector>
 #include <deque>
 
-template <class T> class MutantStack : public  std::stack<T>
+template <class T, class container = std::deque<T> > class MutantStack : public  std::stack<T,container>
 {
     public:
-        typedef typename std::deque<T>::iterator iterator;
+        typedef typename std::conatiner::iterator iterator;
     MutantStack(){}
     MutantStack(const MutantStack &obj){(void)obj;}
     ~MutantStack() {}
     MutantStack &operator=(const MutantStack &obj){(void)obj;return *this;}
     iterator begin()
     {
-        return std::stack<T>::c.begin();
+        return std::stack<T,container>::c.begin();
     }
     iterator end()
     {
-        return std::stack<T>::c.end();
+        return std::stack<T,container>::c.end();
     }
       
 };
--- test/test.cpp
+++ test/test.cpp
@@ -1,16 +1,23 @@
-#include <iostream> 
+#include <iostream>       // std::cout
+#include <stack>          // std::stack
+#include <vector>         // std::vector
+#include <deque>          // std::deque
 
-using namespace std; 
+int main ()
+{
+  std::deque<int> mydeque (3,100);          // deque with 3 elements
+  std::vector<int> myvector (2,200);        // vector with 2 elements
 
-template <typename T> 
-void fun(const T& x) 
-{ 
+  std::stack<int> first;                    // empty stack
+  std::stack<int> second (mydeque);         // stack initialized to copy of deque
 
-cout << ++zi; 
-return; 
-} 
+  std::stack<int,std::vector<int> > third;  // empty stack using vector
+  std::stack<int,std::vector<int> > fourth (myvector);
 
-int main() 
-{	 
-return 0; 
-} 
+  std::cout << "size of first: " << first.size() << '\n';
+  std::cout << "size of second: " << second.size() << '\n';
+  std::cout << "size of third: " << third.size() << '\n';
+  std::cout << "size of fourth: " << fourth.size() << '\n';
+
+  return 0;
+}
