6dcd89a4e349b2455514da798ca08e0ae53d85e9
hznagui <houdayfahouayfa7@gmail.com>
2024-01-24 18:27:42 +0100

    chihaja

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 14:27:28 by hznagui           #+#    #+#             */
-/*   Updated: 2024/01/24 17:29:45 by hznagui          ###   ########.fr       */
+/*   Updated: 2024/01/24 18:27:13 by hznagui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ void parsing_error (int d ,std::string str = "test")
             else if (d==8)
             std::cout << "\033[31mError : too large a number.\033[0m" << std::endl; 
 }
-int number(std::string a)
+int date(std::string a)
 {
     std::string tmp;
     for (size_t i=0;i<a.length();i++)
@@ -88,7 +88,7 @@ int main (int argc,char **argv)
         while (std::getline(database,tmp1,','))
         {
             std::getline(database,tmp,'\n');
-            data[number(tmp1)] = BitcoinExchange(tmp1,std::atof(tmp.c_str()));
+            data[date(tmp1)] = BitcoinExchange(tmp1,std::atof(tmp.c_str()));
         }
         std::getline(file,tmp1,'\n');
         if (std::strcmp(tmp1.c_str(),"date | value"))
@@ -98,8 +98,15 @@ int main (int argc,char **argv)
             if (tmp1.length() < 14 || !std::isdigit(tmp1[0]) || !std::isdigit(tmp1[1]) || !std::isdigit(tmp1[2]) || !std::isdigit(tmp1[3]) ||tmp1[4] != '-' || !std::isdigit(tmp1[5]) || !std::isdigit(tmp1[6]) || tmp1[7] != '-' || !std::isdigit(tmp1[8]) || !std::isdigit(tmp1[9]) || tmp1[10]!=' ' || tmp1[11] != '|' || tmp1[12] != ' ')
                 parsing_error(5,tmp1);
             else if (check_date(tmp1) || check_number(tmp1)){}
-            else 
-                std::cout << tmp1 << std::endl;
+            else
+            {
+                std::map<int , BitcoinExchange>::iterator iter;
+                for (int nbr = date(tmp1.substr(0,10)) ; iter ==  data.end();nbr--)
+                {
+                    iter = data.find(nbr);
+                }
+                std::cout << tmp1.substr(0,10)  << " => " << tmp1.substr(13)<< " = "<< (std::atof (tmp1.substr(13)))*(iter->second.getexchange_rate()) << std::endl;                
+            }
         }
         // std::cout << data[std::atoi(argv[1])].getexchange_rate() << std::endl;
     }
