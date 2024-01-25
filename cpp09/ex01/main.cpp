3ccd4cb0e9026350167f1d1c180718928637cb5b
hznagui <houdayfahouayfa7@gmail.com>
2024-01-25 18:37:53 +0100

    finishing excercice 01

--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 10:30:56 by hznagui           #+#    #+#             */
-/*   Updated: 2024/01/25 16:32:03 by hznagui          ###   ########.fr       */
+/*   Updated: 2024/01/25 18:37:14 by hznagui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,43 @@ int parsing (std::string str)
     }
     return 0;
 }
+int calcul(std::string main)
+{
+    std::stack<int> stack;
+    std::stringstream cstr(main);
+    std::string tmp;
+    int a=0,b=0;
+    while (!cstr.eof())
+    {
+        std::getline(cstr,tmp,' ');
+        if ((!std::strcmp(tmp.c_str(),"-") || !std::strcmp(tmp.c_str(),"+") || !std::strcmp(tmp.c_str(),"/") || !std::strcmp(tmp.c_str(),"*")) && stack.size() <= 1)
+            return 1;
+        else if (std::isdigit(tmp[0]))
+            stack.push(std::atoi(tmp.c_str()));
+        else if (!std::strcmp(tmp.c_str(),"-") || !std::strcmp(tmp.c_str(),"+") || !std::strcmp(tmp.c_str(),"/") || !std::strcmp(tmp.c_str(),"*"))
+        {
+            b = stack.top();
+            stack.pop();
+            a = stack.top();
+            stack.pop();
+            if (!std::strcmp(tmp.c_str(),"-"))
+                stack.push(a - b);
+            else if (!std::strcmp(tmp.c_str(),"+"))
+                stack.push(a + b);
+            else if (!std::strcmp(tmp.c_str(),"/"))
+            {
+                if (b==0)
+                    return 1;
+                stack.push(a / b);  
+            }
+            else if (!std::strcmp(tmp.c_str(),"*"))
+                stack.push(a * b);
+        }
+    }
+    std::cout << "Result => "<< stack.top()<<std::endl;
+    return 0;   
+}
+
 int main(int argc,char **argv)
 {
     if (argc <= 1)
@@ -37,12 +74,12 @@ int main(int argc,char **argv)
         return 1;
     }
     std::string main;
-    for (int i=1;argv[i];i++)
+    for (int i=1;i<argc;i++)
     {
         main += argv[i];
         main += ' ';
     }
-    if (parsing(main) || calcul(main))
+    if (parsing(main) || calcul(main) )
     {
         std::cout << "check ur argement pleaseeeee !!!"<<std::endl;
         return 1;
