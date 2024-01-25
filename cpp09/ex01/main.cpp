91d4415147ddbb2ffaee6243097176709b3b18f8
hznagui <houdayfahouayfa7@gmail.com>
2024-01-25 16:14:21 +0100

    fenishing parsing

--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -6,7 +6,46 @@
 /*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 10:30:56 by hznagui           #+#    #+#             */
-/*   Updated: 2024/01/25 10:30:56 by hznagui          ###   ########.fr       */
+/*   Updated: 2024/01/25 16:14:02 by hznagui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "RPN.hpp"
+
+int parsing (std::string str)
+{
+    for(size_t a = 0 ; a < str.length() ; a++)
+    {
+        if (!isdigit(str[a]) && str[a] != ' '  && str[a] != '+' && str[a] != '-' && str[a] != '*' && str[a] != '/')
+            return 1;
+    }
+    std::stringstream cstr(str);
+    std::string tmp;
+    while (!cstr.eof())
+    {
+        std::getline(cstr,tmp,' ');
+        if (tmp.length()>1)
+            return 1;
+    }
+    return 0;
+}
+int main(int argc,char **argv)
+{
+    if (argc <= 1)
+    {
+        std::cout << "bruh really ??"<<std::endl;
+        return 1;
+    }
+    std::string main;
+    for (int i=1;argv[i];i++)
+    {
+        main += argv[i];
+        main += ' ';
+    }
+    // std::cout << main <<std::endl;
+    if (parsing(main))
+    {
+        std::cout << "check ur argement pleaseeeee !!!"<<std::endl;
+        return 1;
+    }
+}
